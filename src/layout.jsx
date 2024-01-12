23b10cfed715ecda340914ae6bcaebef9c87dbe3
Mast3rLS <lucastoeckl13@gmail.com>
2024-01-12 10:10:40 +0100

    finished site

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,11 +1,13 @@
 import Navigation from "./Navigation/index.js";
 import Body from "./Body/index.js";
+import Footer from "./Footer/index.js";
 
 export default () => {
     return (
         <>
             <Navigation/>
             <Body/>
+            <Footer />
         </>
     );
 }
