aa1c42e0b31d4f9123900f6817e3aff2c31a7542
Mathias Wagner <germannewsmaker@gmail.com>
2024-01-17 20:20:41 +0100

    Updated the layout.jsx

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,25 +1,30 @@
 import Header from "./common/components/Header";
 import Footer from "./common/components/Footer";
+import QuizLoader from "@/states/QuizLoader";
 import FileLoader from "@/states/FileLoader";
 
-
-
-import {useState} from "react";
-import Header2 from "./components/Header2";
-import Footer2 from "./components/Footer2";
-import QuizLoader from "@/states/QuizLoader/index.js";
+import {useContext, useEffect, useState} from "react";
+import {QuizContext} from "@/common/contexts/QuizContext.jsx";
 
 export default () => {
 
-    const [state, setState] = useState("file");
+    const [state, setState] = useState("quiz");
+    const {quiz} = useContext(QuizContext);
 
+    useEffect(() => {
+        if (quiz === null) {
+            setState("file");
+        } else {
+            setState("quiz");
+        }
+    }, [quiz]);
 
     return (
         <>
             <Header/>
             <main>
-                {state === "file" && <FileLoader />}
-                {state === "quiz" && <QuizLoader />}
+                {state === "file" && <FileLoader/>}
+                {state === "quiz" && <QuizLoader/>}
             </main>
             <Footer/>
         </>
