af973cb0946f37d68cd41d48391f20f0069131cc
Mathias Wagner <germannewsmaker@gmail.com>
2024-01-17 20:20:33 +0100

    Updated the QuizLoader.jsx

--- src/states/QuizLoader/QuizLoader.jsx
+++ src/states/QuizLoader/QuizLoader.jsx
@@ -1,28 +1,31 @@
 import "./styles.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faFileArrowUp} from "@fortawesome/free-solid-svg-icons";
-import DragNDrop from "@/common/assets/img/dragndrop.png";
+import {faGamepad} from "@fortawesome/free-solid-svg-icons";
+import {QuizContext} from "@/common/contexts/QuizContext.jsx";
+import {useContext} from "react";
+import QuizInformation from "@/states/QuizLoader/components/QuizInformation";
 
 export const QuizLoader = () => {
+    const {quiz} = useContext(QuizContext);
+
     return (
-        <div className="quiz-container">
+        <div className="fl-wrapper">
+            <input type='file' id='file' style={{display: 'none'}}/>
             <div className="center">
                 <div className="center-left">
                     <h1 className="Greet">
-                            Willkommen zurück, <span className="Name">Herr Brod</span>
+                        Willkommen zurück, <span className="Name">Herr Brod</span>
                     </h1>
                     <p className="Text">
                         Das Quiz wurde geladen. Klicke “Raum öffnen”, um den Raum zu erstellen.
                     </p>
                     <button className="button">
-                        <img src="/public/assets/img/controllerthingy.png" alt=""/>
-                            Raum öffnen
+                        <FontAwesomeIcon className="dateiupload" icon={faGamepad}/>
+                        Raum öffnen
                     </button>
                 </div>
-                <div className="container-right" ref={dropRef}>
-                    <img className="DnD" src={DragNDrop} alt="Raum öffnen"/>
-                    <h2>Das Schulquiz</h2>
-                </div>
+
+                <QuizInformation />
             </div>
         </div>
     );
