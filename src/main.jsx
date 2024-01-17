332f18542ff8d957a163cc48debb4a9396dc8256
Mathias Wagner <germannewsmaker@gmail.com>
2024-01-17 20:20:47 +0100

    Updated the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './layout.jsx';
 import "./common/styles/main.css";
+import {QuizProvider} from "@/common/contexts/QuizContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <QuizProvider>
+          <App />
+      </QuizProvider>
   </React.StrictMode>,
 )
