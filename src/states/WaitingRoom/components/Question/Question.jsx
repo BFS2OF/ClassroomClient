727626b550de9b17a9389f9dbc649592a9eaa42d
Mathias Wagner <germannewsmaker@gmail.com>
2024-01-22 23:25:02 +0100

    Created the Question component (from StudentClient)

---
+++ src/states/WaitingRoom/components/Question/Question.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export const Question = ({title}) => (
+    <div className="question-container">
+        <div className="question-circle"/>
+        <div className="question">
+            <h1>{title}</h1>
+        </div>
+    </div>
+);
