b6737331e37263f129541fb960b39fc01d7462e3
Mathias Wagner <germannewsmaker@gmail.com>
2024-01-22 23:24:15 +0100

    Created the Background component (from StudentClient)

---
+++ src/states/WaitingRoom/components/Background/Background.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export const Background = () => {
+    return (
+        <div className="shape-container">
+            <div/>
+            <div/>
+        </div>
+    );
+}
