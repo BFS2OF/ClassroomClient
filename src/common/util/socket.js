d9c4ed47f074eb955bc96c3e7ee948598d01129f
Mathias Wagner <germannewsmaker@gmail.com>
2024-01-23 13:04:03 +0100

    Updated the socket.js

--- src/common/util/socket.js
+++ src/common/util/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = process.env.NODE_ENV === "production" ? "http://prod.bfs2of.local" : "http://localhost:3000";
+const URL = process.env.NODE_ENV === "production" ? "https://openday.gnmyt.dev/" : "http://localhost:3000";
 
 export const socket = io(URL);
